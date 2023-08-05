1f6d20404f7dab1a33d246bf9da337854cec502e
Yoav Lavi <yoav@yoavlavi.com>
2023-08-05 20:18:35 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Getting started with Rust and WebAssembly";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    inherit (nixpkgs.lib) optional;
+    systems = flake-utils.lib.system;
+  in
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs;
+          [
+            cargo-make
+            deno
+            nodejs
+            nodePackages.prettier
+            rustup
+            wasm-pack
+          ]
+          ++ optional (system == systems.aarch64-darwin) [
+            darwin.apple_sdk.frameworks.Security
+            darwin.apple_sdk.frameworks.CoreFoundation
+            darwin.apple_sdk.frameworks.CoreServices
+          ];
+
+        shellHook = ''
+          export CARGO_INSTALL_ROOT="$(git rev-parse --show-toplevel)/.cargo"
+          export PATH="$CARGO_INSTALL_ROOT/bin:$PATH"
+        '';
+      };
+    });
+}
